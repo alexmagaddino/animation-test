ff9ff920db8ce3468404161e2bf8f554ddbf3a5b
cond72 <alessandro.magaddino@eisworld.it>
2019-03-26 12:49:31 +0100

    new chart section

--- app/src/main/java/it/example/cond72/animationtest/ChartActivity.kt
+++ app/src/main/java/it/example/cond72/animationtest/ChartActivity.kt
@@ -1,7 +1,12 @@
 package it.example.cond72.animationtest
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 /**
  * Created by alexm on 26/03/2019
@@ -11,5 +16,18 @@ class ChartActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart)
+
+        val chart = findViewById<LineChart>(R.id.chart)
+
+        val entries = List(10) {
+            Entry(it * 3.0f, it * 2.5f)
+        }
+
+        val lineDataSet = LineDataSet(entries, "Esperimento").apply {
+            color = Color.RED
+            valueTextColor = Color.BLACK
+        }
+
+        chart.data = LineData(lineDataSet)
     }
 }

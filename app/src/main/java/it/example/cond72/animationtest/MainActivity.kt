dc4cd2296d003c8d5e4140d2dfe89d8586428c37
cond72 <alessandro.magaddino@eisworld.it>
2019-04-01 16:37:50 +0200

    improve with mega crash library

--- app/src/main/java/it/example/cond72/animationtest/MainActivity.kt
+++ app/src/main/java/it/example/cond72/animationtest/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import com.kinda.crash.config.CrashConfig
 
 /**
  * Created by alexm on 25/03/2019
--- app/src/main/java/it/example/cond72/animationtest/TestApplication.kt
+++ app/src/main/java/it/example/cond72/animationtest/TestApplication.kt
@@ -12,18 +12,17 @@ class TestApplication: Application(){
         super.onCreate()
         CrashConfig.Builder.create()
                 .backgroundMode(CrashConfig.BACKGROUND_MODE_SHOW_CUSTOM) //default: CrashConfig.BACKGROUND_MODE_SHOW_CUSTOM
-                .enabled(false) //default: true
+                .enabled(true) //default: true
                 .showErrorDetails(true) //default: true
                 .showRestartButton(true) //default: true
                 .logErrorOnRestart(true) //default: true
                 .trackActivities(true) //default: false
                 .minTimeBetweenCrashesMs(2000) //default: 3000
-//                .errorDrawable(R.drawable.ic_custom_drawable) //default: bug image
+                .errorDrawable(R.drawable.ic_futurama) //default: bug image
                 .restartActivity(MainActivity::class.java) //default: null (your app's launch activity)
 //                .errorActivity(YourCustomErrorActivity::class.java) //default: null (default error activity)
 //                .eventListener(YourCustomEventListener()) //default: null
                 .apply()
-
     }
 
 }
